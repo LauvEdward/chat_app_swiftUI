2f62adc6834666eee3c5e1c33ad887ead418da46
Le Dinh Phuc <phucld@ominext.com>
2023-08-23 09:49:33 +0700

    fix created app

---
+++ chat_app/View/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  chat_app
+//
+//  Created by Mojave on 23/08/2023.
+//
+
+import SwiftUI
+
+struct LoginViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginViewController()
+    }
+}
---
+++ chat_app/chat_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  chat_appApp.swift
+//  chat_app
+//
+//  Created by Mojave on 23/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct chat_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
